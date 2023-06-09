af576c81d2f5547b31ce9fa711f9e56d2e277691
Dani <daniramsahu2001@gmail.com>
2023-06-09 17:33:49 +0530

    Customized pseudo styles and viewports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html","./build/js/*.js"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
         "open-menu": {
